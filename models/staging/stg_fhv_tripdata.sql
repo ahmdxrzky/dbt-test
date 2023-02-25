9bade82a4bb66bde9d0361950d76a9feacafa05d
Ahmad Rizky <ahmad.rizky297474@gmail.com>
2023-02-25 12:17:23 +0000

    adjust fhv

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,32 @@
+{{ config(materialized='view') }}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+)
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    dispatching_base_num,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    cast(sr_flag as numeric) as trip_distance,
+    affiliated_base_number
+from tripdata
+where rn = 1
+
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
